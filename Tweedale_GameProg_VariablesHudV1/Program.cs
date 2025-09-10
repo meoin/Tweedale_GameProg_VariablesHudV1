03314cda94c6e687b1d78174a942196f40d74f52
Michael <hubu99@gmail.com>
2025-09-10 14:59:05 -0300

    HUD v1.1 Methods
    Moved a lot of the code into separate methods to reduce repeated code
    
    Also changed the Kills Multiplier to a Days Multiplier so that players are rewarded for lasting long regardless of fights (which already reward on their own)

--- Tweedale_GameProg_VariablesHudV1/Program.cs
+++ Tweedale_GameProg_VariablesHudV1/Program.cs
@@ -1,30 +1,154 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Tweedale_GameProg_VariablesHudV1
 {
     internal class Program
     {
+        static string name;
+        static float health = 100;
+        static float armor = 0;
+        static int score = 0;
+
+        static bool departed = false;
+        static bool piano = false;
+        static float days = 0;
+
+        static void ShowHUD()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("{0,0}{1,20}{2,20}{3,20}{4,20}", "Character: " + name, "Health: " + health + "/100", "Armor: " + armor, "Score: " + score, "Day " + days);
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void TakeDamage(int damage, bool armorMatters) 
+        {
+            if (armorMatters)
+            {
+                health -= Math.Max((damage - armor), 0);
+            }
+            else 
+            {
+                health -= damage;
+            }
+        }
+
+        static void AddScore(int pointsEarned) 
+        {
+            score += pointsEarned;
+        }
+
+        static void GetFinalScore() 
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("You died!!");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Score: {score}");
+
+            float pianoMultiplier = 1;
+            if (piano == true)
+            {
+                Console.WriteLine("Piano Multiplier: 1.5x");
+                pianoMultiplier = 1.5f;
+            }
+
+
+            float daysMultiplier = 1 + (days / 20);
+            Console.WriteLine($"Days Multiplier: x{daysMultiplier} ({days})");
+
+            float finalScore = score * pianoMultiplier * daysMultiplier;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine($"Final Score: {Math.Floor(finalScore)}");
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void RandomDayEvent(double randomNumber)
+        {
+            if (randomNumber < 0.1)
+            {
+                Console.WriteLine("You found a pile of gold on the ground left from a previous traveller! Yoink!");
+                Console.WriteLine("Score went up by 100!");
+                AddScore(100);
+            }
+            else if (randomNumber < 0.3)
+            {
+                Console.WriteLine("Drats! A beast from the woods charged at you and managed to scratch you up.");
+                Console.WriteLine("You took {0,0} damage but gained 25 score!", Math.Max((10 - armor), 0));
+                TakeDamage(10, true);
+                AddScore(25);
+            }
+            else if (randomNumber < 0.35)
+            {
+                Console.WriteLine("Oh baby, what is this I see? A little extra leather to covet thee?");
+                Console.WriteLine("Advise your foes to flee, as your armor went up not by one but three!");
+                Console.WriteLine("Actually, sorry, it just went by 1. I just wanted to keep rhyming.");
+                armor += 1;
+            }
+            else if (randomNumber < 0.5)
+            {
+                Console.WriteLine("By Jove, a gang of bandits have ambushed you from the night.");
+                Console.WriteLine("You managed to take them out and leave with their loot, but it wasn't easy.");
+                Console.WriteLine("You took {0,0} damage and gained 150 score!", Math.Max((20 - armor), 0));
+                TakeDamage(20, true);
+                AddScore(150);
+            }
+            else if (randomNumber < 0.75)
+            {
+                Console.WriteLine("An uneventful night, but you feel worn down from your travels. Hunger takes its toll.");
+                Console.WriteLine("Your health depletes by 5...");
+                TakeDamage(5, false);
+            }
+            else if (randomNumber < 0.9)
+            {
+                Console.WriteLine("You manage to forage some food from the surrounding bushes, but it doesn't taste very good.");
+                Console.WriteLine("Your health is restored by 10.");
+                health += 10;
+            }
+            else if (randomNumber < 0.95)
+            {
+                Console.WriteLine("By the Gods, you've stumbled upon an inn that's willing to give you shelter! You eat like an animal and sleep like a log.");
+                Console.WriteLine("Your health is fully restored!");
+                health = 100;
+            }
+            else
+            {
+                Console.WriteLine("A giant piano fell from the sky and landed on your head.");
+                Console.WriteLine("You lost 100 health!!! Idiot!");
+                TakeDamage(100, false);
+                piano = true;
+                if (health > 0)
+                {
+                    Console.Write("\nWait... you survived that??? You absolute legend. You open up the piano and stuffed inside you find ");
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("1000 GOLD!");
+                    AddScore(1000);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            string name;
-            float health = 100;
-            float armor = 0;
-            int score = 0;
-            bool departed = false;
-            var rand = new Random();
-            double randomNumber;
+            //string name;
+            //float health = 100;
+            //float armor = 0;
+            //int score = 0;
 
-            bool piano = false;
-            float kills = 0;
+            //bool departed = false;
+            //bool piano = false;
+            //float kills = 0;
+
+            var rand = new Random();
 
             Console.ForegroundColor = ConsoleColor.Green;
-
             Console.Write("Enter your name: ");
-
             Console.ForegroundColor = ConsoleColor.White;
 
             name = Console.ReadLine();
@@ -32,10 +156,10 @@ namespace Tweedale_GameProg_VariablesHudV1
 
             while (health > 0) 
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("{0,0}{1,20}{2,20}{3,20}", "Character: " + name, "Health: " + health + "/100", "Armor: " + armor, "Score: " + score);
 
-                Console.ForegroundColor = ConsoleColor.White;
+                ShowHUD();
+
+                days++;
 
                 if (!departed)
                 {
@@ -44,70 +168,8 @@ namespace Tweedale_GameProg_VariablesHudV1
                 }
                 else
                 {
-                    randomNumber = rand.NextDouble();
-                    //Console.WriteLine(randomNumber);
 
-                    if (randomNumber < 0.1)
-                    {
-                        Console.WriteLine("You found a pile of gold on the ground left from a previous traveller! Yoink!");
-                        Console.WriteLine("Score went up by 100!");
-                        score += 100;
-                    }
-                    else if (randomNumber < 0.3)
-                    {
-                        Console.WriteLine("Drats! A beast from the woods charged at you and managed to scratch you up.");
-                        Console.WriteLine("You took {0,0} damage but gained 25 score!", Math.Max((10 - armor), 0));
-                        health -= Math.Max((10 - armor), 0);
-                        score += 25;
-                        kills++;
-                    }
-                    else if (randomNumber < 0.35)
-                    {
-                        Console.WriteLine("Oh baby, what is this I see? A little extra leather to covet thee?");
-                        Console.WriteLine("Advise your foes to flee, as your armor went up not by one but three!");
-                        Console.WriteLine("Actually, sorry, it just went by 1. I just wanted to keep rhyming.");
-                        armor += 1;
-                    }
-                    else if (randomNumber < 0.5)
-                    {
-                        Console.WriteLine("By Jove, a gang of bandits have ambushed you from the night.");
-                        Console.WriteLine("You managed to take them out and leave with their loot, but it wasn't easy.");
-                        Console.WriteLine("You took {0,0} damage and gained 150 score!", Math.Max((20 - armor), 0));
-                        health -= Math.Max((20 - armor), 0);
-                        score += 150;
-                        kills++;
-                    }
-                    else if (randomNumber < 0.75)
-                    {
-                        Console.WriteLine("An uneventful night, but you feel worn down from your travels. Hunger takes its toll.");
-                        Console.WriteLine("Your health depletes by 5...");
-                        health -= 5;
-                    }
-                    else if (randomNumber < 0.9)
-                    {
-                        Console.WriteLine("You manage to forage some food from the surrounding bushes, but it doesn't taste very good.");
-                        Console.WriteLine("Your health is restored by 10.");
-                        health += 10;
-                    }
-                    else if (randomNumber < 0.95)
-                    {
-                        Console.WriteLine("By the Gods, you've stumbled upon an inn that's willing to give you shelter! You eat like an animal and sleep like a log.");
-                        Console.WriteLine("Your health is fully restored!");
-                        health = 100;
-                    }
-                    else 
-                    {
-                        Console.WriteLine("A giant piano fell from the sky and landed on your head.");
-                        Console.WriteLine("You lost 100 health!!! Idiot!");
-                        health -= 100;
-                        piano = true;
-                        if (health > 0) 
-                        {
-                            Console.Write("\nWait... you survived that??? You absolute legend. You open up the piano and stuffed inside you find ");
-                            Console.ForegroundColor = ConsoleColor.DarkYellow;
-                            Console.WriteLine("1000 GOLD!");
-                        }
-                    }
+                    RandomDayEvent(rand.NextDouble());
 
                 }
 
@@ -119,36 +181,10 @@ namespace Tweedale_GameProg_VariablesHudV1
 
             }
 
-            Console.ForegroundColor = ConsoleColor.Red;
-
-            Console.WriteLine("You died!!");
-
-            Console.ForegroundColor = ConsoleColor.White;
-
-            Console.WriteLine($"Score: {score}");
-
-            float pianoMultiplier = 1;
-
-            if (piano == true)
-            {
-                Console.WriteLine("Piano Multiplier: 1.5x");
-                pianoMultiplier = 1.5f;
-            }
-
-
-            float killsMultiplier = 1 + (kills / 10);
-
-            Console.WriteLine($"Kills Multiplier: x{killsMultiplier} ({kills})");
-
-            float finalScore = score * pianoMultiplier * killsMultiplier;
-
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-
-            Console.WriteLine($"Final Score: {Math.Floor(finalScore)}");
-
-
-            Console.ForegroundColor = ConsoleColor.White;
+            GetFinalScore();
 
         }
     }
 }
+
+
