5d1cd8a89101d135782cc3abf5e0c151c223ddf7
Michael <hubu99@gmail.com>
2025-09-16 15:31:05 -0300

    v1.2 CONDITIONALS - Added weapon types and health status
    -Added an enum for weapons and a scenario where a random weapon is found
    -Weapons do not currently have any gameplay difference yet
    -Health no longer displays as a number, instead it shows as a descriptive status
    -Added input for player to decide if they want to replay or pick up a found weapon

--- Tweedale_GameProg_VariablesHudV1/Program.cs
+++ Tweedale_GameProg_VariablesHudV1/Program.cs
@@ -6,6 +6,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
+enum Weapons 
+{
+    Stick,
+    Sword,
+    Axe,
+    Spear,
+    Mace,
+    Greathammer,
+    Bow,
+    BFG
+}
+
 namespace Tweedale_GameProg_VariablesHudV1
 {
     internal class Program
@@ -13,16 +25,19 @@ namespace Tweedale_GameProg_VariablesHudV1
         static string name;
         static float health = 100;
         static float armor = 0;
+        static Weapons weapon = Weapons.Stick;
         static int score = 0;
 
         static bool departed = false;
         static bool piano = false;
         static float days = 0;
 
+        static Random rand = new Random();
+
         static void ShowHUD()
         {
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("{0,0}{1,20}{2,20}{3,20}{4,20}", "Character: " + name, "Health: " + health + "/100", "Armor: " + armor, "Score: " + score, "Day " + days);
+            Console.WriteLine("{0,0}{1,30}{2,15}{3,20}{4,10}{5,10}", "Character: " + name, "Health: " + HealthStatus(health), "Armor: " + armor, "Weapon: " + weapon, "Day " + days, "Score: " + score);
 
             Console.ForegroundColor = ConsoleColor.White;
         }
@@ -36,6 +51,35 @@ namespace Tweedale_GameProg_VariablesHudV1
             else 
             {
                 health -= damage;
+            }
+        }
+
+        static void Heal(int healing) 
+        {
+            health = Math.Min(health + healing, 100);
+        }
+
+        static string HealthStatus(float hp) 
+        {
+            if (hp >= 100)
+            {
+                return "Perfect Health";
+            }
+            else if (hp > 75)
+            {
+                return "Healthy";
+            }
+            else if (hp > 50) 
+            {
+                return "Hurt";
+            }
+            else if (hp > 10)
+            {
+                return "Badly Hurt";
+            }
+            else
+            {
+                return "Brink of Death";
             }
         }
 
@@ -68,6 +112,31 @@ namespace Tweedale_GameProg_VariablesHudV1
             Console.WriteLine($"Final Score: {Math.Floor(finalScore)}");
 
             Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\nWould you like to play again?");
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("Enter 1 for YES and any other key for NO: ");
+            var input = Console.ReadKey().KeyChar;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            if (input == '1') 
+            {
+                BeginGame();
+            }
+        }
+
+        static Weapons GetRandomWeapon() 
+        {
+            int weaponId = rand.Next(1, 7);
+            
+            Weapons selectedWeapon = (Weapons)weaponId;
+
+            return selectedWeapon;
+        }
+
+        static void ChangeWeapon(Weapons newWeapon) 
+        {
+            weapon = newWeapon;
         }
 
         static void RandomDayEvent(double randomNumber)
@@ -100,23 +169,47 @@ namespace Tweedale_GameProg_VariablesHudV1
                 TakeDamage(20, true);
                 AddScore(150);
             }
-            else if (randomNumber < 0.75)
+            else if (randomNumber < 0.65)
             {
                 Console.WriteLine("An uneventful night, but you feel worn down from your travels. Hunger takes its toll.");
                 Console.WriteLine("Your health depletes by 5...");
                 TakeDamage(5, false);
             }
-            else if (randomNumber < 0.9)
+            else if (randomNumber < 0.75)
             {
                 Console.WriteLine("You manage to forage some food from the surrounding bushes, but it doesn't taste very good.");
                 Console.WriteLine("Your health is restored by 10.");
-                health += 10;
+                Heal(10);
             }
-            else if (randomNumber < 0.95)
+            else if (randomNumber < 0.8)
             {
                 Console.WriteLine("By the Gods, you've stumbled upon an inn that's willing to give you shelter! You eat like an animal and sleep like a log.");
                 Console.WriteLine("Your health is fully restored!");
-                health = 100;
+                Heal(100);
+            }
+            else if (randomNumber < 0.99)
+            {
+                // Find a new weapon
+                Weapons foundWeapon = GetRandomWeapon();
+
+                Console.WriteLine($"On the ground you find a {foundWeapon} in perfectly good condition!");
+                Console.WriteLine($"Would you like to exchange your {weapon} for the {foundWeapon}?");
+
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write("Enter 1 for YES and any other key for NO: ");
+                var input = Console.ReadKey().KeyChar;
+                Console.ForegroundColor = ConsoleColor.White;
+
+                if (input == '1')
+                {
+                    
+                    Console.WriteLine($"\nYou leave behind your {weapon} to arm yourself with the powerful {foundWeapon} and carry on forth!");
+                    ChangeWeapon(foundWeapon);
+                }
+                else 
+                {
+                    Console.WriteLine($"\nYou don't need this {foundWeapon} where you're going. You choose to leave it behind and stick with your existing {weapon}.");
+                }
             }
             else
             {
@@ -134,19 +227,19 @@ namespace Tweedale_GameProg_VariablesHudV1
             }
         }
 
-        static void Main(string[] args)
+        static void BeginGame() 
         {
-
-            var rand = new Random();
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("Enter your name: ");
-            Console.ForegroundColor = ConsoleColor.White;
-
-            name = Console.ReadLine();
             Console.Clear();
 
-            while (health > 0) 
+            health = 100;
+            armor = 0;
+            weapon = Weapons.Stick;
+            score = 0;
+            days = 0;
+            departed = false;
+            piano = false;
+
+            while (health > 0)
             {
 
                 ShowHUD();
@@ -174,7 +267,17 @@ namespace Tweedale_GameProg_VariablesHudV1
             }
 
             GetFinalScore();
+        }
 
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Enter your name: ");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            name = Console.ReadLine();
+
+            BeginGame();
         }
     }
 }
